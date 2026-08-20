8cb7d8000d4f2cdab55a6815f39ea61690977086
sabique <sabiqueislam7@gmail.com>
2026-08-20 21:36:08 +0530

    [refactor] icon imports and update type definitions

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -3,7 +3,10 @@
 import { forwardRef, type ComponentType } from "react";
 import type { LucideProps } from "lucide-react";
 import * as Animated from "@animateicons/react/lucide";
-import { AlertCircle, CheckSquare, Circle, CircleHelp, Columns, Columns3, Cookie, Dot, Eraser, FileJson, FolderDown, FolderUp, GripVertical, Hash, ImagePlus, Languages, Loader2, Maximize2, Palette, PanelLeft, RotateCcw, Rows3, Scissors, Sigma, Square, Table, Table2, Wand2 } from "lucide-react";
+import { Circle, Columns, Columns3, Cookie, Dot, Eraser, GripVertical, Hash, Maximize2, Palette, PanelLeft, Scissors, Sigma, Square } from "lucide-react";
+
+/** AnimateIcons wrap a div; LucideProps.ref is SVGSVGElement. Drop ref so the types match. */
+export type IconProps = Omit<LucideProps, "ref">;
 
 function iconSize(className?: string, size?: LucideProps["size"]) {
   if (typeof size === "number") return size;
@@ -12,8 +15,8 @@ function iconSize(className?: string, size?: LucideProps["size"]) {
   return 16;
 }
 
-function wrapIcon(Icon: ComponentType<any>): ComponentType<LucideProps> {
-  const Wrapped = forwardRef<HTMLDivElement, LucideProps>((props, ref) => {
+function wrapIcon(Icon: ComponentType<any>) {
+  const Wrapped = forwardRef<HTMLDivElement, IconProps>((props, ref) => {
     const { className, size, color, ...rest } = props;
     return (
       <Icon
@@ -26,7 +29,7 @@ function wrapIcon(Icon: ComponentType<any>): ComponentType<LucideProps> {
     );
   });
   Wrapped.displayName = "AnimatedIcon";
-  return Wrapped as ComponentType<LucideProps>;
+  return Wrapped;
 }
 
 export const AlignCenter = wrapIcon(Animated.AlignCenter);
@@ -40,22 +43,29 @@ export const Bold = wrapIcon(Animated.Bold);
 export const BookOpen = wrapIcon(Animated.BookOpen);
 export const Calculator = wrapIcon(Animated.Calculator);
 export const Calendar = wrapIcon(Animated.Calendar);
+export const CalendarCheck = wrapIcon(Animated.CalendarCheck);
 export const Check = wrapIcon(Animated.Check);
 export const ChevronDown = wrapIcon(Animated.ChevronDown);
 export const ChevronLeft = wrapIcon(Animated.ChevronLeft);
 export const ChevronRight = wrapIcon(Animated.ChevronRight);
 export const ChevronUp = wrapIcon(Animated.ChevronUp);
+export const CircleCheck = wrapIcon(Animated.CircleCheck);
 export const Cloud = wrapIcon(Animated.Cloud);
 export const CloudOff = wrapIcon(Animated.CloudOff);
 export const Code = wrapIcon(Animated.Code);
 export const Copy = wrapIcon(Animated.Copy);
+export const Cpu = wrapIcon(Animated.Cpu);
 export const CreditCard = wrapIcon(Animated.CreditCard);
 export const Download = wrapIcon(Animated.Download);
 export const ExternalLink = wrapIcon(Animated.ExternalLink);
 export const Eye = wrapIcon(Animated.Eye);
 export const EyeOff = wrapIcon(Animated.EyeOff);
+export const FileCode = wrapIcon(Animated.FileCode);
 export const FilePlus = wrapIcon(Animated.FilePlus);
+export const FileSpreadsheet = wrapIcon(Animated.FileSpreadsheet);
 export const FileText = wrapIcon(Animated.FileText);
+export const Flag = wrapIcon(Animated.Flag);
+export const Flame = wrapIcon(Animated.Flame);
 export const FolderOpen = wrapIcon(Animated.FolderOpen);
 export const Github = wrapIcon(Animated.Github);
 export const Globe = wrapIcon(Animated.Globe);
@@ -64,9 +74,12 @@ export const Heading1 = wrapIcon(Animated.Heading1);
 export const Heading2 = wrapIcon(Animated.Heading2);
 export const History = wrapIcon(Animated.History);
 export const Image = wrapIcon(Animated.Image);
+export const Info = wrapIcon(Animated.Info);
 export const Italic = wrapIcon(Animated.Italic);
 export const Keyboard = wrapIcon(Animated.Keyboard);
 export const LayoutGrid = wrapIcon(Animated.LayoutGrid);
+export const LayoutList = wrapIcon(Animated.LayoutList);
+export const Lightbulb = wrapIcon(Animated.Lightbulb);
 export const Link = wrapIcon(Animated.Link);
 export const List = wrapIcon(Animated.List);
 export const ListChecks = wrapIcon(Animated.ListChecks);
@@ -85,6 +98,8 @@ export const Plug = wrapIcon(Animated.Plug);
 export const Plus = wrapIcon(Animated.Plus);
 export const Quote = wrapIcon(Animated.Quote);
 export const RefreshCw = wrapIcon(Animated.RefreshCw);
+export const Repeat = wrapIcon(Animated.Repeat);
+export const Rocket = wrapIcon(Animated.Rocket);
 export const Save = wrapIcon(Animated.Save);
 export const Search = wrapIcon(Animated.Search);
 export const Send = wrapIcon(Animated.Send);
@@ -98,48 +113,52 @@ export const Type = wrapIcon(Animated.Type);
 export const Underline = wrapIcon(Animated.Underline);
 export const Upload = wrapIcon(Animated.Upload);
 export const User = wrapIcon(Animated.User);
+export const Wallet = wrapIcon(Animated.Wallet);
 export const X = wrapIcon(Animated.X);
+
+export const AlertCircle = wrapIcon(Animated.TriangleAlert);
 export const AlertTriangle = wrapIcon(Animated.TriangleAlert);
+export const CheckSquare = wrapIcon(Animated.CircleCheck);
+export const CircleHelp = wrapIcon(Animated.Info);
 export const Code2 = wrapIcon(Animated.CodeXml);
 export const FileDown = wrapIcon(Animated.Download);
+export const FileJson = wrapIcon(Animated.FileCode);
 export const FilePlus2 = wrapIcon(Animated.FilePlus);
+export const FolderDown = wrapIcon(Animated.Download);
+export const FolderUp = wrapIcon(Animated.Upload);
 export const Heading3 = wrapIcon(Animated.Heading);
 export const ImageIcon = wrapIcon(Animated.Image);
+export const ImagePlus = wrapIcon(Animated.Image);
+export const Languages = wrapIcon(Animated.Globe);
 export const Layout = wrapIcon(Animated.LayoutDashboard);
 export const Link2Off = wrapIcon(Animated.Unlink);
 export const LinkIcon = wrapIcon(Animated.Link);
+export const Loader2 = wrapIcon(Animated.LoaderCircle);
+export const LoaderCircle = wrapIcon(Animated.LoaderCircle);
 export const MoreHorizontal = wrapIcon(Animated.Ellipsis);
 export const PenLine = wrapIcon(Animated.Pencil);
 export const PenTool = wrapIcon(Animated.Compass);
+export const RotateCcw = wrapIcon(Animated.TimerReset);
+export const Rows3 = wrapIcon(Animated.LayoutList);
 export const Share2 = wrapIcon(Animated.Share);
+export const Table = wrapIcon(Animated.FileSpreadsheet);
+export const Table2 = wrapIcon(Animated.FileSpreadsheet);
 export const Unplug = wrapIcon(Animated.PlugZap);
-export { AlertCircle };
-export { CheckSquare };
+export const Wand2 = wrapIcon(Animated.Sparkles);
+
 export { Circle };
-export { CircleHelp };
 export { Columns };
 export { Columns3 };
 export { Cookie };
 export { Dot };
 export { Eraser };
-export { FileJson };
-export { FolderDown };
-export { FolderUp };
 export { GripVertical };
 export { Hash };
-export { ImagePlus };
-export { Languages };
-export { Loader2 };
 export { Maximize2 };
 export { Palette };
 export { PanelLeft };
-export { RotateCcw };
-export { Rows3 };
 export { Scissors };
 export { Sigma };
 export { Square };
-export { Table };
-export { Table2 };
-export { Wand2 };
 
-export type { LucideProps } from "@/lib/icons";
+export type { LucideProps };
