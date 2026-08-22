f2405b4c6935277d1d708b3bc22e3dbac3ec8daa
sabique <sabiqueislam7@gmail.com>
2026-08-23 00:34:35 +0530

    [feat] ImageLightboxHost component into main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { EditorAppearanceProvider } from "./components/EditorAppearanceProvider";
+import { ImageLightboxHost } from "./components/ImageLightboxHost";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <EditorAppearanceProvider>
       <App />
+      <ImageLightboxHost />
     </EditorAppearanceProvider>
   </ThemeProvider>
 );
